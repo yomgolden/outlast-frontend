9fa4c87f31ea5bf6cfa50bdeba26664652f88f12
yomgolden <yomadeogun@gmail.com>
2026-05-11 11:26:47 +0100

    Update MatchContext.jsx

--- src/context/MatchContext.jsx
+++ src/context/MatchContext.jsx
@@ -1,1 +1,28 @@
+import {
+  createContext,
+  useContext,
+  useState
+} from "react";
 
+const MatchContext = createContext();
+
+export const MatchProvider = ({
+  children
+}) => {
+
+  const [match, setMatch] = useState(null);
+
+  return (
+    <MatchContext.Provider
+      value={{
+        match,
+        setMatch
+      }}
+    >
+      {children}
+    </MatchContext.Provider>
+  );
+};
+
+export const useMatch = () =>
+  useContext(MatchContext);
