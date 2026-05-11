41f5a6eb7590af55e52db4abe32f867a63dc79d1
yomgolden <yomadeogun@gmail.com>
2026-05-11 12:18:16 +0100

    Update MatchContext.jsx

--- src/context/MatchContext.jsx
+++ src/context/MatchContext.jsx
@@ -4,19 +4,36 @@ import {
   useState
 } from "react";
 
-const MatchContext = createContext();
+const MatchContext =
+  createContext();
 
 export const MatchProvider = ({
   children
 }) => {
 
-  const [match, setMatch] = useState(null);
+  const [match, setMatch] =
+    useState(null);
+
+  const [feed, setFeed] =
+    useState([]);
+
+  const [results, setResults] =
+    useState(null);
+
+  const [loading, setLoading] =
+    useState(false);
 
   return (
     <MatchContext.Provider
       value={{
         match,
-        setMatch
+        setMatch,
+        feed,
+        setFeed,
+        results,
+        setResults,
+        loading,
+        setLoading
       }}
     >
       {children}
