92c06d271eb579302cb22bdbc772fe58bebc1dfe
yomgolden <yomadeogun@gmail.com>
2026-05-12 21:57:27 +0100

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,17 +7,54 @@ import Match from "../pages/Match";
 import Results from "../pages/Results";
 import Leaderboard from "../pages/Leaderboard";
 import Profile from "../pages/Profile";
+import History from "../pages/History";
 
 export default function AppRoutes() {
+
   return (
+
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/events" element={<Events />} />
-      <Route path="/queue" element={<Queue />} />
-      <Route path="/match" element={<Match />} />
-      <Route path="/results" element={<Results />} />
-      <Route path="/leaderboard" element={<Leaderboard />} />
-      <Route path="/profile" element={<Profile />} />
+
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+      <Route
+        path="/events"
+        element={<Events />}
+      />
+
+      <Route
+        path="/queue"
+        element={<Queue />}
+      />
+
+      <Route
+        path="/match"
+        element={<Match />}
+      />
+
+      <Route
+        path="/results"
+        element={<Results />}
+      />
+
+      <Route
+        path="/leaderboard"
+        element={<Leaderboard />}
+      />
+
+      <Route
+        path="/history"
+        element={<History />}
+      />
+
+      <Route
+        path="/profile"
+        element={<Profile />}
+      />
+
     </Routes>
   );
 }
