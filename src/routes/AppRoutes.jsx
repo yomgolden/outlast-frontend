9379e0eb5d0fb197d749850bc4060f77cc2aab72
yomgolden <yomadeogun@gmail.com>
2026-05-12 20:06:42 +0100

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,57 +5,25 @@ import {
 } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Events from "../pages/Events";
 import Queue from "../pages/Queue";
 import Match from "../pages/Match";
 import Results from "../pages/Results";
 import Leaderboard from "../pages/Leaderboard";
 import Profile from "../pages/Profile";
-import Shop from "../pages/Shop";
 
 export default function AppRoutes() {
-
   return (
     <BrowserRouter>
-
       <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/queue"
-          element={<Queue />}
-        />
-
-        <Route
-          path="/match"
-          element={<Match />}
-        />
-
-        <Route
-          path="/results"
-          element={<Results />}
-        />
-
-        <Route
-          path="/leaderboard"
-          element={<Leaderboard />}
-        />
-
-        <Route
-          path="/profile"
-          element={<Profile />}
-        />
-
-        <Route
-          path="/shop"
-          element={<Shop />}
-        />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/queue" element={<Queue />} />
+        <Route path="/match" element={<Match />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-
     </BrowserRouter>
   );
 }
