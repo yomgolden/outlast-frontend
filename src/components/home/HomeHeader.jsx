8917084e677c4d414e3a8b8695be02665da2d0a4
yomgolden <yomadeogun@gmail.com>
2026-05-16 22:39:53 +0100

    Create HomeHeader.jsx

---
+++ src/components/home/HomeHeader.jsx
@@ -0,0 +1,27 @@
+export default function HomeHeader() {
+
+  return (
+
+    <div
+      style={{
+        marginBottom: 18
+      }}
+    >
+
+      <div className="game-title">
+        OUTLAST
+      </div>
+
+      <div
+        style={{
+          color: "var(--text3)",
+          fontSize: 12,
+          marginTop: 2
+        }}
+      >
+        Nigerian survival simulator
+      </div>
+
+    </div>
+  );
+}
