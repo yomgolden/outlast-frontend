99c86577774eb29f4d2408284b7cd5d4cd790011
yomgolden <yomadeogun@gmail.com>
2026-05-16 16:53:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,6 +46,14 @@ const [showSplash, setShowSplash] = useState(true);
 
   }, []);
 
+  if (showSplash) {
+  return (
+    <SplashScreen
+      onFinish={() => setShowSplash(false)}
+    />
+  );
+}
+
   return (
 
     <div className="shell">
